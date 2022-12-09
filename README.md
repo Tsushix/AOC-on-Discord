19fdc8fffba27362599b17e4164ec20d919e1cad
Tsushix <leo.jc@outlook.fr>
2022-12-09 02:24:19 +0100

    optimise + upgrade system

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 # AOC on Discord
 
-This easier program to obtain your private AOC leaderboard on your favorite Discord server !
+The easiest way to get your private AOC leaderboard on your favorite Discord server !
 
-To configure the program, open `config.json` file and edit fields.
+To configure the program, open `config.json` file and edit the fields.
 
 
 ## webhookURL
 Webhook URL which is on your Discord Server, to fint it:  
 `Server settings => Integrations => Create Webhook => Copy URL`
 
-## leaderboardLINK
-Link which is on your adress bar when you are on your private leaderboard, it look like this:  
-`https://adventofcode.com/20xx/leaderboard/private/view/xxxxxxx`
+## leaderboardID
+Last numbers which is on your adress bar when you are on your private leaderboard, it should look like this:  
+`https://adventofcode.com/20xx/leaderboard/private/view/__XXXXXXX__`
 
 ## sessionID
 Cookie's value which manage your login on Advent Of Code. To find it:  
@@ -21,6 +21,5 @@ Cookie's value which manage your login on Advent Of Code. To find it:
 Time interval after which a new leaderboard is sent (in minutes)
 
 
-Then, launch `index.js` file with a daemon or directly with node.
+Then, launch `index.js` file with a daemon or directly with node (don't forget to install the project dependencies)
 The first leaderboard will be sent after the time you typed in `config.json`.
-
--- index.js
+++ index.js
@@ -1,32 +1,74 @@
 
 const puppeteer = require("puppeteer");
+const https = require("https");
 const Discord = require("discord.js");
-const sizeOf = require("image-size");
-const { webhookURL, sessionID, leaderboardLINK, timeInterval } = require("./config.json");
+const { webhookURL, sessionID, leaderboardID, timeInterval } = require("./config.json");
 
-console.log(sessionID)
 const web = new Discord.WebhookClient({ url: webhookURL });
-console.log("Lancé !");
-(async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+if (timeInterval < 1) return console.log("[!] Please, renseign a timeInterval highter than 1 !")
 
-    await page.goto(leaderboardLINK);
-    await page.setCookie({name: "session", value: sessionID});
-    await page.goto(leaderboardLINK);
+const options = {
+    hostname: "adventofcode.com",
+    path: `/${new Date().getFullYear()}/leaderboard/private/view/${leaderboardID}.json`,
+    method: "GET",
+    headers: {'Cookie': `session=${sessionID}`}
+    }
 
-    setInterval(async () => {
-        var height = sizeOf(await page.screenshot({fullPage: true})).height;
-        var width = sizeOf(await page.screenshot({fullPage: true})).width;
-        await page.screenshot({clip: {x: 0, y:286, height: height - 320, width: width - 430}}).then(image => {
-        const attachment = new Discord.AttachmentBuilder(image, { name: `leaderboard.png`})
-        web.send({embeds: [new Discord.EmbedBuilder()
-            .setColor("#303434")
-            .setImage(`attachment://leaderboard.png`)
-            .setDescription(`**Leaderboard du ${new Date().getDate()}/${new Date().getMonth() + 1}/${new Date().getFullYear()}, à ${new Date().getHours()}:${new Date().getMinutes()}**`)
-        ],files: [attachment]})
-    })
-    }, timeInterval*1000);
+https.get(options, res => {
+    res.on("data", data => manageData(data))
+    res.on("error", err => console.log(err))
+});
 
+setInterval(() => {
+    https.get(options, res => {
+        res.on("data", data => manageData(data))
+        res.on("error", err => console.log(err))
+    });
+}, 1000*60*timeInterval);
+
+
+function manageData(data){
+
+    if (data?.toString().startsWith("<!DOCTYPE")) return console.log("[!] Leaderboard was not found, change your config and try again !")
+
+    let orderID = {}
+
+    try {data = JSON.parse(data.toString())} catch (error) {throw error;}
     
-})()
+    for (const member in data.members) orderID[data.members[member].name] = [data.members[member].local_score, data.members[member].stars]
+    var score = Object.keys(orderID).map(key => [key, orderID[key]])
+    score.sort((a,b) => b[1][0] - a[1][0])
+
+    var top = 1
+    var date = new Date().getDate()
+    let fields = {"top": "", "score": "", "stars": ""}
+    if(0 > new Date().getHours() < 6) date-=1
+    score.forEach(user => {
+        fields["top"]+=`**${top})**\n`
+        fields["score"]+=`**${user[1][0]}**\n`
+        for (let i = 0; i < Math.floor(user[1][1]/2); i++) fields["stars"]+="★"
+        if (user[1][1]%2 != 0) fields["stars"]+="✯"
+        for (let i = 0; i < date - Math.floor(user[1][1]/2) - user[1][1]%2; i++) fields["stars"]+='☆'
+        if(user[0] == 'null') fields["stars"]+=`　**anonymous**\n`
+        else fields["stars"]+=`　**${user[0]}**\n`
+        top++
+    })
+
+    var hours = new Date().getHours()
+    var minutes = new Date().getMinutes()
+
+    if (hours.toLocaleString().length < 2) hours = "0" + hours.toLocaleString()
+    if (minutes.toLocaleString().length < 2) minutes = "0" + minutes.toLocaleString()
+    
+    web.send({embeds: [new Discord.EmbedBuilder()
+        .setColor("#303434")
+        .setTitle(`Leaderboard of ${new Date().getDate()}/${new Date().getMonth()}/${new Date().getFullYear()}, at ${hours}:${minutes}`)
+        .addFields(
+            {name: "‏‏‎ ‎", value: fields["top"], inline: true},
+            {name: "‏‏‎ ‎", value: fields["score"], inline: true},
+            {name: "‏‏‎ ‎", value: fields["stars"], inline: true},
+        )
+    ]})
+
+    console.log("Running !");
+}
